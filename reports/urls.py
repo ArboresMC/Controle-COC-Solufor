057f022fe646515c0bf01ce9e6c6e0478e022d74
ArboresMC <solufor.certificacao@gmail.com>
2026-03-18 16:22:35 -0300

    Delete reports directory

--- reports/urls.py
+++
@@ -1,6 +0,0 @@
-from django.urls import path
-from .views import ConsolidatedExcelReportView
-
-urlpatterns = [
-    path('consolidated.xlsx', ConsolidatedExcelReportView.as_view(), name='report_consolidated_excel'),
-]
