92830aeeeba0be3bab223a3a5130ab597bc22ffb
ArboresMC <solufor.certificacao@gmail.com>
2026-03-18 14:47:07 -0300

    Update build.sh

--- build.sh
+++ build.sh
@@ -5,3 +5,11 @@ pip install -r requirements.txt
 
 python manage.py collectstatic --noinput
 python manage.py migrate
+
+# cria superusuário automático
+python manage.py shell << END
+from django.contrib.auth import get_user_model
+User = get_user_model()
+if not User.objects.filter(username="admin").exists():
+    User.objects.create_superuser("admin", "admin@email.com", "12345678")
+END
