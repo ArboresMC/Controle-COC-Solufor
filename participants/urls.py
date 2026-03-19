b8ddbf2a0ba8c1f8d5f93c53e500c80adbde5fd8
ArboresMC <solufor.certificacao@gmail.com>
2026-03-19 13:21:31 -0300

    Delete participants directory

--- participants/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from .views import ParticipantListView, ParticipantCreateView, ParticipantUpdateView
-
-urlpatterns = [
-    path('', ParticipantListView.as_view(), name='participant_list'),
-    path('new/', ParticipantCreateView.as_view(), name='participant_create'),
-    path('<int:pk>/edit/', ParticipantUpdateView.as_view(), name='participant_update'),
-]
