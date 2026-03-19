d2c77cf3d797ec375f864a59bc25b36998391762
ArboresMC <solufor.certificacao@gmail.com>
2026-03-19 15:42:21 -0300

    Delete accounts directory

--- accounts/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from .views import UserListView, UserCreateView, UserUpdateView
-
-urlpatterns = [
-    path('', UserListView.as_view(), name='user_list'),
-    path('new/', UserCreateView.as_view(), name='user_create'),
-    path('<int:pk>/edit/', UserUpdateView.as_view(), name='user_update'),
-]
