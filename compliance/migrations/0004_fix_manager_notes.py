31f3f96b09a47b7f44027c8a4bb94f5f6ff9a870
Eduardo Nadolny <solufor.certificacao@gmail.com>
2026-04-09 10:40:13 -0300

    fix: manager_notes default vazio + rotas compliance + meses no select

---
+++ compliance/migrations/0004_fix_manager_notes.py
@@ -0,0 +1,20 @@
+from django.db import migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('compliance', '0003_add_missing_columns'),
+    ]
+
+    operations = [
+        migrations.RunSQL(
+            sql="""
+            ALTER TABLE compliance_monthlyclosing
+              ALTER COLUMN manager_notes SET DEFAULT '';
+            UPDATE compliance_monthlyclosing
+              SET manager_notes = '' WHERE manager_notes IS NULL;
+            """,
+            reverse_sql=migrations.RunSQL.noop,
+        ),
+    ]
