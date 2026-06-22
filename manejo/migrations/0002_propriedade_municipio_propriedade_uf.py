faf4f58c3b907e49cb71d585062d938583e0d8f6
Eduardo Nadolny <solufor.certificacao@gmail.com>
2026-06-22 14:44:20 -0300

    fix: corrige dependencia da migration de municipio/uf

--- manejo/migrations/0002_propriedade_municipio_propriedade_uf.py
+++ manejo/migrations/0002_propriedade_municipio_propriedade_uf.py
@@ -6,7 +6,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('manejo', '0002_bootstrap_propriedades_fm'),
+    ('manejo', '0001_initial'),
     ]
 
     operations = [
